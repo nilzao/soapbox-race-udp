09e04f77467592593cd7dfc75e52a3bd1c5eef3d
Nilzao <nilzao@gmail.com>
2016-05-27 01:02:32 -0400

    clean comment

--- src/main/java/br/com/xht/udp/handler/IUdpHello.java
+++ src/main/java/br/com/xht/udp/handler/IUdpHello.java
@@ -2,8 +2,6 @@ package br.com.xht.udp.handler;
 
 public interface IUdpHello {
 
-	// public IUdpHello getNewInstance();
-
 	public IUdpTalk startTalk(UdpDataPacket dataPacket);
 
 	public byte getSessionClientIdx();
